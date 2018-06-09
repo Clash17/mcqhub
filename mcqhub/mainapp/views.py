5bae8f628ad7b08a8a6070d92d0a6d3ef0f457af
Amey Deshpande <ameydeshpande30@gmail.com>
2018-06-09 20:12:29 +0530

    first commit

--- mcqhub/mainapp/views.py
+++ mcqhub/mainapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def loginView(request):
+    return render(request, "login.html")
+
+
+def takeinput(request):
+    mno = request.post["mobilenum"]
+    print(mno)
+    return HttpResponse("hello")
--- mcqhub/mcqhub/urls.py
+++ mcqhub/mcqhub/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.loginView),
+    path('submit/', views.submitData),
 ]
